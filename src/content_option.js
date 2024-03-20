5be12590bee208d1553a1ed8ff671ef364f15e8a
gkp96 <gautam2002pandeyjee@gmail.com>
2024-03-20 16:50:36 +0530

    added

---
+++ src/content_option.js
@@ -0,0 +1,161 @@
+const logotext = "Gautam";
+const meta = {
+    title: "Gautam Pandey",
+    description: "I’m Gautam Pandey Full stack devloper,final year student persuing Bachelors in Information Technolgy",
+};
+
+const introdata = {
+    title: "I’m Gautam Pandey",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Hi, I'm Gautam Kumar Pandey, currently pursuing my Bachelors in Technology from Muzaffarpur Institute of Technology. I'm specializing in Information Technology. My skills include Java, Javascript, Html, Css, Node.js Express, git & github and mongodb. In my most recent role as a software developer intern at deep thought, I had the opportunity to work on the backend of an e-commerce web application, utilizing technologies such as Node.js, Express.js and mongodb. My responsibilities include creating order apis, wishlist apis payment integration using Razorpay, development of the admin panel using React.js, and deploying the application on Aws S3. My notable projects include the development of the Junoon website, a web app designed for the cultural fest of MIT. I created the MARK MY ATTENDANCE web app, which efficiently tracks the attendance of thousands of engineering and polytechnic students in Bihar.",
+    your_img_url: "https://i.imgur.com/28cE8YG.jpeg",
+};
+
+const dataabout = {
+    title: " about me",
+    aboutme: "Hi, I'm Gautam Kumar Pandey, currently pursuing my Bachelors in Technology from Muzaffarpur Institute of Technology. I'm specializing in Information Technology. My skills include Java, Javascript, Html, Css, Node.js Express, git & github and mongodb. In my most recent role as a software developer intern at deep thought, I had the opportunity to work on the backend of an e-commerce web application, utilizing technologies such as Node.js, Express.js and mongodb. My responsibilities include creating order apis, wishlist apis payment integration using Razorpay, development of the admin panel using React.js, and deploying the application on Aws S3. My notable projects include the development of the Junoon website, a web app designed for the cultural fest of MIT. I created the MARK MY ATTENDANCE web app, which efficiently tracks the attendance of thousands of engineering and polytechnic students in Bihar.",
+};
+// const worktimeline = [{
+//         jobtitle: "Designer of week",
+//         where: "YAdfi",
+//         date: "2020",
+//     },
+//     {
+//         jobtitle: "Designer of week",
+//         where: "Jamalya",
+//         date: "2019",
+//     },
+//     {
+//         jobtitle: "Designer of week",
+//         where: "ALquds",
+//         date: "2019",
+//     },
+// ];
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Spring Boot",
+        value: 85,
+    },
+    {
+        name:"Kafka",
+        value : 80
+    },
+    {
+        name:"Microservices",
+        value : 80
+    },
+    {
+        name:"Redis",
+        value: 80
+    },
+    {
+        name:"SQL",
+        value: 80
+    },
+    {
+        name:"Advance Java",
+        value: 80
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name:"Node.js",
+        value:90
+    },
+    {
+        name:"Express",
+        value: 80
+    },
+    {
+        name:"MongoDB",
+        value: 80
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name:"Python",
+        value:70
+    },
+    
+];
+
+const services = [{
+        title: "Web Apps",
+        description: "Our web app development service brings your ideas to life on the digital platform. We specialize in creating dynamic and interactive web applications tailored to your specific needs. Whether it's an e-commerce platform, a social networking site, or a business management tool, we leverage cutting-edge technologies to deliver scalable, secure, and user-friendly solutions",
+    },
+    {
+        title: "UI and UX Design",
+        description: "Our UI/UX design service focuses on crafting visually stunning and intuitive interfaces that enhance user engagement and satisfaction. We combine creativity with user-centered design principles to deliver designs that not only look great but also provide seamless navigation and usability. Whether it's a website, mobile app, or software interface, we prioritize user experience to drive success.",
+    },
+    {
+        title: "Poster Design",
+        description: "Elevate your brand presence and capture attention with our professional poster design service. We specialize in creating eye-catching posters that effectively communicate your message and captivate your target audience. From promotional posters to event announcements, we leverage creative design elements and strategic messaging to deliver visually compelling posters that leave a lasting impression.",
+
+    }
+];
+
+const dataportfolio = [{
+        img: "https://i.imgur.com/Tzzbv9H.jpeg",
+        description: "Full Stack web Application created for cultural event Fiesta of our college, Using Web app thousand of students registered in the event. Technology used MERN-Stack",
+        link: "https://fiestamit.in",
+    },
+    {
+        img: "https://i.imgur.com/Kh04a3j.jpeg",
+        description: "Full stack web application created for attendance management of lakh of engineering and polytechnique students of Bihar. Technology used: MERN-Stack",
+        link: "https://markmyattendance.in",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Full Stack web application created to store and verify data of nptel examinations",
+        link: "#",
+    },
+    {
+        img: "https://i.imgur.com/WZHfnzk.jpeg",
+        description: "Poster created by me using Canva",
+        link: "https://fiestamit.in/acting-drama",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "gautam2002pandeyjee@gmail.com",
+    YOUR_FONE: "+919128931330",
+    description: `Ready to explore collaborative opportunities or have questions about my work? Let's connect! I'm Gautam Kumar Pandey, a proactive B.Tech student specializing in Information Technology from Muzaffarpur Institute of Technology. With expertise in Java, JavaScript, HTML, CSS, Node.js, Express, Git, GitHub, and MongoDB, I've honed my skills through hands-on experience. During my recent role as a software developer intern at Deep Thought, I contributed to the backend development of an e-commerce web application using Node.js, Express.js, and MongoDB. Notable projects include creating the Junoon website for MIT's cultural fest and developing the MARK MY ATTENDANCE web app, facilitating efficient attendance tracking for thousands of students. With over 700 problem-solving instances on Geeks for Geeks and currently serving as the President of MIT IT WORKING group, I'm dedicated to leveraging technology to solve real-world challenges. Feel free to reach out to discuss potential collaborations or learn more about my work!
+     Looking forward to connecting,
+      Gautam Kumar Pandey`,
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_2cshmeq",
+    YOUR_TEMPLATE_ID: "template_v0d85h5",
+    YOUR_USER_ID: "l89J7OLlpJS2NUISN",
+};
+
+const socialprofils = {
+    github: "https://github.com/GKP96",
+    instagram: "gkp1.5",
+    linkedin: "https://www.linkedin.com/in/gautam-kumar-pandey-6a1644208/",
+    twitter: "https://twitter.com/GPandey9496",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    // worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
